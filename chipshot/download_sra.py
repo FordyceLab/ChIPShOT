f9b4fe06366361f0994ed3de560ad80e69e5bad6
Tyler Shimko <TShimko126@gmail.com>
2016-03-09 13:55:47 -0800

    Change GSM ID to sample ID

--- chipshot/download_sra.py
+++ chipshot/download_sra.py
@@ -7,13 +7,13 @@ from Bio import Entrez
 import xml.etree.ElementTree as ET
 
 
-def search(gsm, email, dump_fq, dump_sam, output):
+def search(sample_id, email, dump_fq, dump_sam, output):
     """
-    Search the Sequence Read Archive for the given GSM ID(s) provided by the
+    Search the Sequence Read Archive for the given sample ID(s) provided by the
     user and download the .sra files for all runs associated with that ID
 
     Args:
-        gsm (str) - the GSM id to search for in the SRA database
+        sample_id (str) - the sample ID to search for in the SRA database
         email (str) - the user's email address (required to search the db)
         dump_fq (bool) - whether to dump the fastq files after downloading the
                          .sra file
@@ -27,24 +27,24 @@ def search(gsm, email, dump_fq, dump_sam, output):
     # Set the emial
     Entrez.email = email
 
-    # Print the GSM ID being searched
-    print('Searching for GSM "{gsm}"'.format(gsm=gsm))
+    # Print the sample ID being searched
+    print('Searching for sample ID "{}"'.format(sample_id))
 
-    # Search the Sequence Read Archive database for the GSM ID
-    handle = Entrez.esearch(db="sra", term=gsm)
+    # Search the Sequence Read Archive database for the sample ID
+    handle = Entrez.esearch(db="sra", term=sample_id)
 
-    # Get the ID number for the GSM
+    # Get the ID number for the sample
     ids = Entrez.read(handle)['IdList']
 
-    # Exit if the GSM search has no hits or more than one hit
+    # Exit if the sample search has no hits or more than one hit
     if len(ids) == 0:
-        print('No records matching GSM search term: "{gsm}"'.format(gsm=gsm))
+        print('No records matching ID search term: "{}"'.format(sample_id))
         return
     if len(ids) > 1:
-        print('GSM search term "{gsm}" is ambiguous'.format(gsm=gsm))
+        print('ID search term "{}" is ambiguous'.format(sample_id))
         return
 
-    # Retrieve further information about the GSM search
+    # Retrieve further information about the sample search
     handle = Entrez.efetch(db="sra", id=ids[0])
 
     # Parse the returned XML
@@ -52,9 +52,9 @@ def search(gsm, email, dump_fq, dump_sam, output):
     root = tree.getroot()
     runs = root.find("EXPERIMENT_PACKAGE").find("RUN_SET").findall("RUN")
 
-    # Print a warning if the GSM has multiple sequencing runs
+    # Print a warning if the sample has multiple sequencing runs
     if len(runs) > 1:
-        print('Multiple runs found for GSM ID "{gsm}"'.format(gsm=gsm))
+        print('Multiple runs found for sample ID "{}"'.format(sample_id))
 
     # Make the output directory
     dirname = "./{}".format(output)
@@ -118,7 +118,7 @@ def download(accession, outfile):
 
 def main(argv):
     """
-    Run the search and download subroutines for the GSM ID specified by the
+    Run the search and download subroutines for the sample ID specified by the
     user in the command line areguments
 
     Args:
@@ -147,7 +147,7 @@ def main(argv):
         print("Please provide a sample ID")
         sys.exit(1)
 
-    # For each GSM ID, search and download
+    # For each sample ID, search and download
     for sample_id in args.ids:
         search(sample_id, email, args.fastq, args.sam, args.output[0])
 
